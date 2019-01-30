b37f065c01d77b1bb12475d79429e68aaacea9a8
coderzstas <coderz35@gmail.com>
2019-01-30 15:33:41 +0300

    upd

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -1,7 +1,7 @@
 # LineageOS System Version
 ADDITIONAL_BUILD_PROPERTIES += \
     ro.lineage.version=$(LINEAGE_VERSION) \
-    ro.lineage.releasetype=NIGHTLY \
+    ro.lineage.releasetype=$(LINEAGE_BUILDTYPE) \
     ro.lineage.build.version=$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR) \
     ro.modversion=$(LINEAGE_VERSION) \
     ro.lineagelegal.url=https://lineageos.org/legal
@@ -12,8 +12,8 @@ ADDITIONAL_BUILD_PROPERTIES += \
 
 # LineageOS Platform SDK Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    safecallsos 
+    lineage.updater.uri=https://safecallsos.com/LineageOTA/api/v1/{device}/{type}/{incr}
 
 # LineageOS Platform Internal Version
 ADDITIONAL_BUILD_PROPERTIES += \
-    safecallsos
+    lineage.updater.uri=https://safecallsos.com/LineageOTA/api/v1/{device}/{type}/{incr}
